a4f7008f170239bdc07ecda9648413b7379aa6c0
RabbitTone <[jose02579@gmail.com]>
2024-09-23 16:01:38 -0600

    submission

--- casa_8.py
+++ casa_8.py
@@ -1,1 +1,17 @@
+import pandas as pd
 
+df = pd.read_csv('big-mac-full-index.csv')
+
+print("\nIterating over rows using iterrows() method :\n")
+
+# Using iterrows() method
+for index, row in df.iterrows():
+    print(row["date"],row["iso_a3"],row["currency_code"],row["name"])
+
+print("\nIterating over rows using apply() method :\n")
+
+# Using apply() method
+print(df.apply(lambda row: row['name'] +  " "  + 
+               str(row['local_price']) +  " "  + 
+               str(row['dollar_ex']) +  " "  + 
+               str(row['dollar_price']), axis=1))
